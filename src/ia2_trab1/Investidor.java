b5bc4933b514ae337e2a9d7958443f5498602639
AlessandraH <alehairi@gmail.com>
2017-10-07 10:31:54 -0300

    funcao de predicao feita

--- src/ia2_trab1/Investidor.java
+++ src/ia2_trab1/Investidor.java
@@ -2,6 +2,7 @@ package ia2_trab1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  *
@@ -63,33 +64,117 @@ public class Investidor {
         return probabilidades;
     }
     
+    public boolean sucesso(Float prob) {
+        return new Random().nextInt(101) > prob;
+    }
+    
     public Float probCond(Float probA, Float probB) {
         return (probA * probB)/probB;
     }
     
-    public ArrayList<Float> atualizaProb(ArrayList<Float> probabilidades, Integer SD, int CV) {
-        for (int i = 0; i < 4; i++) {
-            if (i < 2) {
-                probabilidades.set(i, probCond(probabilidades.get(i), probabilidades.get(SD)));
-            } else {
-                probabilidades.set(i, probCond(probabilidades.get(i), probabilidades.get(CV)));
+    public ArrayList<Float> atualizaSubDesc(ArrayList<Float> probabilidades, Integer SD) {
+        switch (SD) {
+            case 0: {
+                for (int i = 0; i < 4; i++)
+                    probabilidades.set(i, probCond(probabilidades.get(i), probabilidades.get(0)));
+                break;
+            }
+            case 1: {
+                for (int i = 0; i < 4; i++) 
+                    probabilidades.set(i, probCond(probabilidades.get(i), probabilidades.get(1)));
+                break;
+            }
+            default: {
+                for (int i = 0; i < 4; i++)
+                    probabilidades.set(i, probCond(probabilidades.get(i), 1 - (probabilidades.get(0) + probabilidades.get(1))));
             }
         }
         return probabilidades;
     }
-    
+        
     public Float predicao(Empresa empresa, ArrayList<Float> probabilidades) throws Exception {
         int nroAcoes = 0; 
         float saldo = 0.0f;
-        
+        boolean bom, regular, ruim, comprou, vendeu;
         Leitor l = new Leitor();
         
         empresa.registro = l.interpretar(l.getHistoricoEmpresa(empresa.getNome(), 1996));
-        
         Arrays.sort(empresa.registro.toArray());
         
         for (Registro r : empresa.registro) {
+            comprou = sucesso(probabilidades.get(2));
+            vendeu = sucesso(probabilidades.get(3));
+            if (probabilidades.get(0) > probabilidades.get(1)) {
+                bom = true;
+                regular = false;
+                ruim = false;
+            } else if (probabilidades.get(0) < probabilidades.get(1)) {
+                bom = false;
+                regular = false;
+                ruim = true;
+            } else {
+                bom = false;
+                regular = true;
+                ruim = false;
+            }
             
+            if (nroAcoes == 0) {
+                if (bom && comprou) {
+                    nroAcoes++;
+                    saldo -= (float) r.precoOfc;
+                    atualizaSubDesc(probabilidades, 0);
+                } else if (regular && comprou) {
+                    if (saldo >= 0){
+                        nroAcoes++;
+                        saldo -= (float) r.precoOfc;
+                        atualizaSubDesc(probabilidades, 0);
+                    } else {
+                        atualizaSubDesc(probabilidades, -1);
+                    }
+                } else if (ruim) {
+                    atualizaSubDesc(probabilidades, 1);
+                } else {
+                    atualizaSubDesc(probabilidades, -1);
+                }
+            } else {
+                if (bom) {
+                    if (comprou) {
+                        if (vendeu) {
+                            saldo += (float) r.precoOfv;
+                            nroAcoes--;
+                        }
+                        saldo -= (float) r.precoOfc;
+                        nroAcoes++;
+                    } else {
+                        if (vendeu) {
+                            saldo += (float) r.precoOfv;
+                            nroAcoes--;
+                        }
+                    }
+                    atualizaSubDesc(probabilidades, 0);
+                } else if (ruim) {
+                    if (vendeu) {
+                        saldo += (float) r.precoOfv;
+                        nroAcoes--;
+                    }
+                    atualizaSubDesc(probabilidades, 1);
+                } else {
+                    if (comprou) {
+                        if (vendeu) {
+                            saldo += (float) r.precoMed;
+                            nroAcoes--;
+                        }
+                        saldo -= (float) r.precoMed;
+                        nroAcoes++;
+                    } else {
+                        if (vendeu) {
+                            saldo += (float) r.precoMed;
+                            nroAcoes--;
+                        }
+                    }
+                    atualizaSubDesc(probabilidades, -1);
+                }
+            }
         }
         System.out.println(nroAcoes);
         System.out.println("SALDO: " + saldo);
